5933d0afaad3f357476b26b7a21aeb3f9ed9a378
luayidriss <idrissluay@gmail.com>
2023-08-04 11:46:06 +0000

    Initial Commit

---
+++ vegan_burgers/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for vegan_burgers project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vegan_burgers.settings')
+
+application = get_asgi_application()
