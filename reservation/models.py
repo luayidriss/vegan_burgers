d40ca4b5b3fe4f7b3c712c49b301261fcb042bb4
luayidriss <idrissluay@gmail.com>
2023-08-06 14:05:03 +0000

    Create Database Models

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    guests = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return f"{self.user.username} - {self.date} {self.time} - {self.guests}"
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length= 50)
+    description = models.CharField(max_length= 300)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    item_image = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return f"{self.name} - {self.description} - {self.price}"
