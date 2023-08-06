83c6aecd21d88985a11e993faea232c2d2cb9074
luayidriss <idrissluay@gmail.com>
2023-08-06 14:46:45 +0000

    Connect Databases to Admin Panel

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Reservation, Menu_Item
 
-# Register your models here.
+class ReservationAdmin(admin.ModelAdmin):
+
+    list_display = ('user', 'date', 'time', 'guests')
+    search_fields = ['user']
+admin.site.register(Reservation, ReservationAdmin)
+
+
+class MenuItemAdmin(admin.ModelAdmin):
+    
+    list_display = ('name', 'price')
+    search_fields = ['name']
+admin.site.register(Menu_Item, MenuItemAdmin)
--- reservation/models.py
+++ reservation/models.py
@@ -15,7 +15,7 @@ class Reservation(models.Model):
         return f"{self.user.username} - {self.date} {self.time} - {self.guests}"
 
 
-class Menu(models.Model):
+class Menu_Item(models.Model):
     name = models.CharField(max_length= 50)
     description = models.CharField(max_length= 300)
     price = models.DecimalField(max_digits=5, decimal_places=2)
